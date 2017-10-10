7e3a7e80cd7b5f5a98f5f1278c11eb32bd31df81
Dezerae Cox <dezeraecox@gmail.com>
2017-10-10 14:19:56 +1100

    Initiating the calc file with simple print statement

---
+++ calc.py
@@ -0,0 +1,15 @@
+import sys
+
+
+print (sys.argv)
+'''
+filename, num1, op, num2 = sys.argv
+
+num1, num2 = int(num1), int(num2)
+
+if op == "+":
+    print (num1 + num2)
+
+elif op == "-":
+    print (num1 - num2)
+'''
